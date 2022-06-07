5a637cad4d3e28f539ef15e1c676ba1003470212
Luis Augusto Jarillo <luis@neotix.com.br>
2022-06-07 13:16:42 -0300

    criada a rota para login e a requisição ajax do form de login

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="{{ url(mix('backend/assets/css/login.css')) }}"/>
     <link rel="icon" type="image/png" href="backend/assets/images/favicon.png"/>
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>UpAdmin - Site Control</title>
 </head>
 <body>
@@ -23,7 +25,7 @@
                 <h1>Login</h1>
             </header>
 
-            <form name="login" action="" method="post" autocomplete="off">
+            <form name="login" action="{{ route('admin.login.do') }}" method="post" autocomplete="off">
                 <label>
                     <span class="field icon-envelope">E-mail:</span>
                     <input type="email" name="email" placeholder="Informe seu e-mail" required/>
