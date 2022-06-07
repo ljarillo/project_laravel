570c5e885f50028c959cb50d6dd24aeef20588a6
Luis Augusto Jarillo <luis@neotix.com.br>
2022-06-07 12:33:30 -0300

    Organizando os caminhos dos assets da dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function (){
 
     Route::get('/', 'AuthController@showLoginForm')->name('login');
+    Route::get('home', 'AuthController@home')->name('home');
 });
